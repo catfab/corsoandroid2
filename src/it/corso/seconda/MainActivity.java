86e838af886d8559a28c04df903632932fa211df
Fabrizio Cattaneo <cat.fab@tin.it>
2013-10-10 15:02:06 +0200

    Marker

--- src/it/corso/seconda/MainActivity.java
+++ src/it/corso/seconda/MainActivity.java
@@ -1,22 +1,47 @@
 package it.corso.seconda;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	
+		  static final LatLng MILANO = new LatLng(53.558, 9.927);
+		  static final LatLng FIORI = new LatLng(53.551, 9.993);
+		  private GoogleMap map;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+		  @Override
+		  protected void onCreate(Bundle savedInstanceState) {
+		    super.onCreate(savedInstanceState);
+		    setContentView(R.layout.activity_main);
+		    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.mappa1))
+		        .getMap();
+		    Marker hamburg = map.addMarker(new MarkerOptions().position(MILANO)
+		        .title("Milano"));
+		    Marker kiel = map.addMarker(new MarkerOptions()
+		        .position(FIORI)
+		        .title("MilanoFiori")
+		        .snippet("Qui il corso")
+		        .icon(BitmapDescriptorFactory
+		            .fromResource(R.drawable.ic_launcher)));
 
-}
+		    // Move the camera instantly to hamburg with a zoom of 15.
+		    map.moveCamera(CameraUpdateFactory.newLatLngZoom(MILANO, 15));
+
+		    // Zoom in, animating the camera.
+		    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		  }
+
+		 
+
+		} 
+
